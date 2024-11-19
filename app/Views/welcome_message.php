19c317c7759d3ddee339868ce24d5dac1787371e
JSediii <sedeejulcastro@gmail.com>
2024-11-20 07:59:46 +0800

    Fix: fragment & .env

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -1,15 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Welcome to CodeIgniter 4!</title>
-    <meta name="description" content="The small framework with powerful features">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/png" href="/favicon.ico">
+<?php
 
-    <!-- STYLES -->
-
-<body>
-
-</body>
-</html>
+use CodeIgniter\Database\BaseUtils;
+?>
+<?= $this->extend('layout/main_layout') ?>
+<?= $this->section('title') ?>
+<title>Imperial Treasures</title>
+<?= $this->endSection('title') ?>
+<?= $this->section('content') ?>
+<p>Meow</p>
+<?= $this->endSection('content') ?>
