988f142946c7d4fa797c41fb402ea38eb4a7081d
cenTimo13 <timobernardo13@gmail.com>
2024-11-20 10:01:20 +0800

    style: enhance dashboard table layout with Bootstrap classes

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -8,25 +8,27 @@ use CodeIgniter\Database\BaseUtils;
 <?= $this->endSection('title') ?>
 <?= $this->section('content') ?>
 
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Content</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php if($tb_promotional): ?>
-            <?php foreach ($tb_promotional as $row) : ?>
-                <tr>
-                    <td><?php echo $row['id'] ?></td>
-                    <td><?php echo $row['title'] ?></td>
-                    <td><?php echo $row['content'] ?></td>
-                </tr>
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </tbody>
-</table>
+<div class="d-flex justify-content-center">
+    <table class="table table-striped table-bordered w-75">
+        <thead class="thead-dark">
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Content</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if($tb_promotional): ?>
+                <?php foreach ($tb_promotional as $row) : ?>
+                    <tr>
+                        <td><?php echo $row['id'] ?></td>
+                        <td><?php echo $row['title'] ?></td>
+                        <td><?php echo $row['content'] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </tbody>
+    </table>
+</div>
 
 <?= $this->endSection('content') ?>
